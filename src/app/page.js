e0f33b3fe2fa7f804bfe9fe04fb1f90172385255
NYUCT Design Labs <132071678+nyuct@users.noreply.github.com>
2025-10-09 13:42:22 +0530

    mobile responsive

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ import DtfImage from "@/component/DtfImage";
 import DTFDetail from "@/component/DTFDetail";
 export default function Home() {
   return (
-    <div className="w-10/12 mx-auto md-flex justify-between pt-10 relative">
-      <div className="md-w-2/4 ">
+    <div className="w-10/12 mx-auto md-flex flex justify-between pt-10 relative">
+      <div className="md-w-2/4 w-2/4 ">
         <DtfImage />
       </div>
-      <div className="md-w-2/4 pt-10  ">
+      <div className="md-w-2/4 w-2/4 md-pt-10  ">
         <DTFDetail />
       </div>
     </div>
